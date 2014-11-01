b23d02974acfedf2a78be52bd299a08434d63238
derdiedasjojo <pi@raspberrypi.(none)>
2014-11-01 10:18:27 +0000

    Driver works as it should now

--- src/Driver.py
+++ src/Driver.py
@@ -2,23 +2,28 @@ from Stepper import Stepper
 from time import sleep
 
 class Driver():
-  def __init__(self,time=0.01):
-    self.time=0.005
-    self.left = Stepper(19,20,21,26,time=time)
-    self.right = Stepper(5,6,12,13,time=time)
+  def __init__(self, time=0.01):
+    self.time=0.01
+    #current robot setup
+    self.left = Stepper(14, 15, 18, 23, time)
+    self.right = Stepper(22, 27, 17, 4, time)
+    #setup for lcd
+    #self.left = Stepper(19,20,21,26,time=time)
+    #self.right = Stepper(5,6,12,13,time=time)
 
-  def forward(self):
-    for i in range(0,7):
+  def forwards(self):
+    for i in range(0,8):
+      #print("left step: {}, right step: {}".format(i,i))
       self.left.Step(i,True)
       self.right.Step(i,True)
       sleep(self.time)
       self.left.Step(i,False)
       self.right.Step(i,False)
       sleep(self.time)
-      #print i
 
   def backwards(self):
-    for i in range(7,0,-1):
+    for i in range(7,-1,-1):
+      #print("left step: {}, right step: {}".format(i,i))
       self.left.Step(i,True)
       self.right.Step(i,True)
       sleep(self.time)
@@ -27,20 +32,22 @@ class Driver():
       sleep(self.time)
 
   def rotateRight(self):
-    i2=range(7,0,-1)
-    for i in range(0,7):
+    i2=range(7,-1,-1)
+    for i in range(0,8):
+      #print("left step: {}, right step: {}".format(i,i2[i]))
       self.left.Step(i,True)
-      self.left.Step(i2[i],True)
+      self.right.Step(i2[i],True)
       sleep(self.time)
       self.left.Step(i,False)
       self.right.Step(i2[i],False)
       sleep(self.time)
 
-  def rotateRight(self):
-    i2=range(7,0,-1)
-    for i in range(0,7):
+  def rotateLeft(self):
+    i2=range(7,-1,-1)
+    for i in range(0,8):
+      #print("left step: {}, right step: {}".format(i2[i],i))
       self.left.Step(i2[i],True)
-      self.left.Step(i,True)
+      self.right.Step(i,True)
       sleep(self.time)
       self.left.Step(i2[i],False)
       self.right.Step(i,False)
@@ -49,4 +56,6 @@ class Driver():
 if __name__ =="__main__":
   d=Driver();
   for i in range(0,100):
-    d.forward()
+    #d.forwards()
+		d.backwards()
+		#d.rotateLeft()
